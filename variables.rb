f039cff0f138e7803021f19ac71d6d0e023351e5
Dara Estrada <daraestrada91@gmail.com>
2021-10-22 22:17:02 -0500

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,12 @@
+famous_person = "Alec Baldwin"
+is_active = true
+
+famous_person = "Missy Elliott"
+
+num_3 = 9
+num_9 = 3
+sum = num_9 + num_3
+
+animals = ["hippopotamous", "fairy", "unicorn"]
+remove_first_element = animals.shift
+how_many_characters = famous_person.length
