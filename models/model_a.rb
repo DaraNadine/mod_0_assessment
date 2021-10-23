4cb15f0621ea8f5987b7645704a19bdea5155a3f
Dara Estrada <daraestrada91@gmail.com>
2021-10-22 22:25:23 -0500

    Add airport hash model

---
+++ models/model_a.rb
@@ -0,0 +1,12 @@
+airport = {
+
+  popular_airline: "United",
+  least_popuar: "Southwest",
+  total_number_of_terminals: 4,
+  total_number_of_gates: 91,
+  has_liquor_license: true,
+  has_children_playground: false,
+  restaurants: ["Bennigans", "Chilis", "Pappas"],
+  bars: ["Lil Woodrows", "Tony's", "Cadillac Bar"]
+  
+}
