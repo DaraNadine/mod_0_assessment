672cd25681a6f9e777c822719b6826fc10c698aa
Dara Estrada <daraestrada91@gmail.com>
2021-10-22 22:31:35 -0500

    Add vehicle hash

---
+++ models/model_b.rb
@@ -0,0 +1,12 @@
+vehicle = {
+
+  make: "Chevrolet",
+  model: "Silverado",
+  number_of_engine_cylinders: 8,
+  number_of_tires: 4,
+  is_truck_on: false,
+  is_door_unlocked: false,
+  interior_features: ["heated seats", "Bluetooth"],
+  exterior_features: ["4x4 sticker", "power tail gate"]
+
+}
